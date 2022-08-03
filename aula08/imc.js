52124de147cb034a2847c62de893603907d570d9
@Gustavo-Almeida-M <gustavoam916@gmail.com>
2022-08-02 22:33:54 -0300

    Mais exercicios e teste.

---
+++ aula08/imc.js
@@ -0,0 +1,5 @@
+const nome = "Fernando";
+const peso = 80;
+const altura = 1.80;
+let imc = peso / (altura * altura);
+console.log(`Seu IMC é ${imc}.`);
---
+++ aula09/operadores.js
@@ -0,0 +1,28 @@
+let valor1 = 10;
+let valor2 = 2;
+
+let resultado;
+
+resultado = valor1 + valor2;
+console.log(resultado);
+
+resultado = valor1 - valor2;
+console.log(resultado);
+
+resultado = valor1 * valor2;
+console.log(resultado);
+
+resultado = valor1 / valor2;
+console.log(resultado);
+
+resultado = valor1 % valor2;
+console.log(resultado);
+
+resultado = valor1 ** valor2;
+console.log(resultado);
+
+let numero1 = parseInt('5.6');
+console.log(numero1);
+
+let numero2 = parseFloat('5.6');
+console.log(numero2);
