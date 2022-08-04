f0dbfd56d0b7d725ec9f024b3be9d89e30561ed7
@Gustavo-Almeida-M <gustavoam916@gmail.com>
2022-08-04 09:56:12 -0300

    teste com números

--- aula10/js/index.js
+++ aula10/js/index.js
@@ -1,7 +1,8 @@
 //alert('Cavalo');
 // confirm('Vc tem certeza que deseja excluir ?');
-let nome = prompt("Digite seu nome.");
-console.log(nome);
+// let nome = prompt("Digite seu nome.");
+// console.log(nome);
 
-let idade = parseInt(prompt("Digite sua idade."));
-console.log(idade);
+// let idade = parseInt(prompt("Digite sua idade."));
+// console.log(idade);
+
---
+++ aula12/numeros.js
@@ -0,0 +1,17 @@
+let a = 10;
+let b = 5;
+
+console.log(a.toString() + b);
+//a /= 3;
+console.log(a.toFixed(2));
+
+let comp = (Number.isInteger(a));
+
+if(comp)
+{
+    console.log("É um número inteiro!");
+}
+else
+{
+    console.log("Não é um número inteiro!");
+}
