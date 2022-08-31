06260e86e2b78609815ac83ec261f4fc3b6ec5ee
@Gustavo-Almeida-M <gustavoam916@gmail.com>
2022-08-31 15:04:12 -0300

    modelo html

---
+++ aula20/ifelse.js
@@ -0,0 +1,22 @@
+let hora = 4;
+
+if(hora > 24)
+{
+    console.log("Não é uma hora valida!");
+}
+else if(hora < 5)
+{
+    console.log("Boa madrugada");
+}
+else if(hora < 12)
+{
+    console.log("Bom dia");
+}
+else if(hora < 17)
+{
+    console.log("Boa tarde")
+}
+else
+{
+    console.log("Boa noite");
+}
