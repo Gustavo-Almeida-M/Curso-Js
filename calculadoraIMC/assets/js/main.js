44ddc5babfbaa0726f31ed422e08e1b30b796d7a
@Gustavo-Almeida-M <gustavoam916@gmail.com>
2022-09-29 22:19:11 -0300

    avanços na calculadora

--- calculadoraIMC/assets/js/main.js
+++ calculadoraIMC/assets/js/main.js
@@ -2,5 +2,37 @@ const form = document.querySelector('#formulario');
 
 form.addEventListener('submit', function (e){
     e.preventDefault();
-    console.log('evento');
+    
+    const inputPeso = e.target.querySelector('.peso');
+    const inptAltura = e.target.querySelector('.altura');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inptAltura.value);
+
+    if(peso && altura)
+    {
+        console.log(peso, altura);
+    }
+    else
+    {
+        console.log("Não é um número!");
+    }
 });
+
+function criaP()
+{
+    const p = document.createElement('p');
+    return p; 
+}
+
+function resultado(msg)
+{
+    const valor = document.querySelector('.valor');
+    valor.innerHTML = '';
+
+    const p = criaP();
+    p.classList.add('resultado-bom');
+
+    p.innerHTML = msg;
+    valor.appendChild(p);
+}
