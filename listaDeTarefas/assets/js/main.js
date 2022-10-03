56bb12496f395ba5ed7231261c31535e9af9b2bf
@Gustavo-Almeida-M <gustavoam916@gmail.com>
2022-10-03 18:57:03 -0300

    começo do código sobre lista de tarefas

---
+++ listaDeTarefas/assets/js/main.js
@@ -0,0 +1,37 @@
+const form = document.querySelector('.formulario');
+
+form.addEventListener('submit', function(informacoes)
+{
+    informacoes.preventDefault();
+
+    const titulo = informacoes.target.querySelector('.tituloTarefa');
+    const descricao = informacoes.target.querySelector('.descricaoTarefa');
+
+    imprimir(titulo, descricao);
+});
+
+function criaTitulo()
+{
+    const nomeTitulo = document.createElement('h1');
+
+    return nomeTitulo;
+}
+function criaDescricao()
+{
+    const nomeDescricao = document.createElement('p');
+
+    return nomeDescricao;
+}
+function imprimir(titulo, descricao)
+{
+    const tarefa = document.querySelector('.localTarefa');
+    tarefa.innerHTML = '';
+
+    const imprimirTitulo = criaTitulo();
+    const imprimirDescricao = criaDescricao();
+
+    imprimirTitulo.innerHTML = titulo;
+    imprimirDescricao.innerHTML = descricao;
+
+    tarefa.appendChild(imprimirTitulo);
+}
