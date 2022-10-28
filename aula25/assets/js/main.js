b4d7aecc232397c82354088e87f4369936e5949b
@Gustavo-Almeida-M <gustavoam916@gmail.com>
2022-10-28 16:02:35 -0300

    Terminando a lista com for

--- aula25/assets/js/main.js
+++ aula25/assets/js/main.js
@@ -1,14 +1,26 @@
 const lista = 
 [
+    //Criando a lista com os elementos html e o texto
     {tag: 'div', texto: "Carro 1"},
     {tag: 'p', texto: "Carro 2"},
     {tag: 'header', texto: "Carro 3"},
     {tag: 'footer', texto: "Carro 4"}
 ];
 
+//Escolhendo o elemento no arquivo html
+const container = document.querySelector('.container');
+const div = document.createElement('div');
+
 for(let i = 0; i < lista.length; i++)
 {
+    //Desconstruindo o array
     let {tag, texto} = lista[i];
-
-    console.log(tag, texto);
+    //Passando qual tag sera criada
+    let elementoCriado = document.createElement(tag);
+    //Passando o texto
+    elementoCriado.innerHTML = texto;
+    //Acionando o elemento
+    div.appendChild(elementoCriado);
 }
+//Imprimindo os elementos
+container.appendChild(div);
