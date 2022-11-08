11fb795b8987ccbad855c9faa3776790067c82dd
Gustavo Almeida <gustavoam916@gmail.com>
2022-11-07 21:47:16 -0300

    Usando for in

---
+++ aula25/forIn.js
@@ -0,0 +1,18 @@
+// const Nome = ['Gustavo', 'Caio', 'João', 'Felipe'];
+
+// for(let nomes in Nome)
+// {
+//     console.log(nomes);
+// }
+
+const Pessoa = 
+{
+    nome: 'Gustavo',
+    idade: 49,
+    cidade: 'Pedra Branca'
+}
+
+for(let index in Pessoa)
+{
+    console.log(index, Pessoa[index]);
+}
