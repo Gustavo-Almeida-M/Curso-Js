99044c86efbe6d57ae1f4462ffb91ef2ec4ec1b1
Gustavo Almeida <gustavoam916@gmail.com>
2023-01-22 12:06:02 -0300

    recomeçando a lista de tarefas

---
+++ listaDeTarefas/assets/js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('submit', function(e)
+{
+    e.preventDefault();
+
+    const recebeTitulo = document.querySelector('titulo');
+    const recebeDescricao = document.querySelector('descricao');
+
+    let titulo = recebeTitulo
+})
