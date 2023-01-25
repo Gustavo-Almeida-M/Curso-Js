9c59ca0e348edb238b35a962abef29e1822a5550
Gustavo Almeida <gustavoam916@gmail.com>
2023-01-25 09:47:28 -0300

    Criando um teste

---
+++ atv/atividades/testeDeVariavel.js
@@ -0,0 +1,11 @@
+//Criando uma variavel global
+const global = 10;
+
+function retornNumero()
+{
+    //Atribuindo valor a uma variavel mesmo sem definila
+    variavel = global;
+    console.log(variavel);
+}
+
+retornNumero();
