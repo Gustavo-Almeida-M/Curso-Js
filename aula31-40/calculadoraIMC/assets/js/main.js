bac16ad8f38a3c1f806ae632c8a853dc66f60d4d
@Gustavo-Almeida-M <gustavoam916@gmail.com>
2023-07-13 15:36:33 -0300

    criando uma calculadora de imc

---
+++ aula31-40/calculadoraIMC/assets/js/main.js
@@ -0,0 +1,12 @@
+let operacao = document.querySelector('#calcular');
+let peso = document.querySelector('#peso');
+let altura = document.querySelector('#altura');
+
+operacao.addEventListener( "submit" , function(e)
+{
+    e.preventDefault();
+    
+    
+});
+
+let resultado = document.querySelector('.resultado')
