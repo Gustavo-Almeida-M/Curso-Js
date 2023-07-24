0d9678a1a1248ba5e91b17287bb8acd3dfc37692
@Gustavo-Almeida-M <gustavoam916@gmail.com>
2023-07-24 10:46:09 -0300

    Lista de tarefas

---
+++ aula31-40/aula31/index.js
@@ -0,0 +1,6 @@
+function maxValue(num1, num2)
+{
+    return num1 >= num2 ? num1: num2; 
+}
+
+console.log(maxValue(9, 25)); 
--- projetos/listaDeTarefas/assets/js/main.js
+++ projetos/listaDeTarefas/assets/js/main.js
@@ -1,9 +1,104 @@
-document.addEventListener('submit', function(e)
+const tarefa = document.querySelector('.tarefa');
+const btAdicionarTarefa = document.querySelector('.btAdicionarTarefa');
+const lista = document.querySelector('.lista');
+
+//função para criar um elemento li html
+function criaLi()
 {
-    e.preventDefault();
+    return document.createElement('li');
+}
 
-    const recebeTitulo = document.querySelector('titulo');
-    const recebeDescricao = document.querySelector('descricao');
+//Adiciona um texto no li escolhido
+function adicionarTarefa(msg)
+{
+    const li = criaLi();
+    li.innerText = msg;
 
-    let titulo = recebeTitulo
-})
+    addButton(li);
+
+    lista.appendChild(li);  
+}
+
+//Adiciona um tarefa na lista
+function criaTarefa()
+{
+    if(!tarefa.value) return;
+    adicionarTarefa(tarefa.value);
+    salvaTarefas();
+    limpaCampo();
+}
+
+//Função auxiliar, serve apenas para limpar o campo de texto e colocar o foco nele
+function limpaCampo()
+{
+    tarefa.value = '';
+    tarefa.focus();
+}
+
+//Função auxiliar, serve para adicionar um botão junto do li
+function addButton(li)
+{
+    li.innerText += ' ';
+    const button = document.createElement('button');
+    button.innerText = 'Apagar';
+    button.setAttribute('class', 'apagar')
+    li.appendChild(button);
+}
+
+//Função para que quando for digitado enter ele executara uma adição na lista
+tarefa.addEventListener('keypress', function(e)
+{
+    if(e.keyCode === 13)
+    {
+        criaTarefa();
+    }
+});
+
+//Adiciona uma tarefa quando for clicado no botão
+btAdicionarTarefa.addEventListener('click', function(e)
+{
+    criaTarefa();
+});
+
+//Função de apagar um item da lista de tarefa
+document.addEventListener('click', function(e)
+{
+    const elemento = e.target;
+
+    if(elemento.classList.contains('apagar'))
+    {
+        elemento.parentElement.remove();
+        salvaTarefas();
+    }
+});
+
+//Salva as tarefas no localStorage
+function salvaTarefas()
+{
+    const liTarefas = lista.querySelectorAll('li');
+    const listaTarefas = [];
+
+    for(let i of liTarefas)
+    {
+        let textoLi = i.innerText;
+        textoLi = textoLi.replace(' Apagar', '');
+        listaTarefas.push(textoLi);
+    }
+    
+    const dadosJSON = JSON.stringify(listaTarefas);
+    localStorage.setItem('tarefasSalvas', dadosJSON);
+
+}
+
+//Busca a lista anterior se tiver
+function adicionaTarefasSalvas()
+{
+    const tarefas = localStorage.getItem('tarefasSalvas');
+    const listaTarefas = JSON.parse(tarefas);
+
+    for(let i of listaTarefas)
+    {
+        adicionarTarefa(i);
+    }
+}
+adicionaTarefasSalvas();
